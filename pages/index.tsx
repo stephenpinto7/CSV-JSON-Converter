15e8ae8bb7e77147722508e0c1504c28955aebac
Stephen Pinto <spinto7@hotmail.com>
2022-08-25 14:10:54 -0400

    Added basic JSON validation and changed branding.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import '@fontsource/inconsolata';
 import Head from 'next/head';
-import { AppBar, Card, CardHeader, CardContent, Container, FormGroup, FormControlLabel, Grid, Select, type SelectChangeEvent, Switch, TextField, Toolbar, Typography, MenuItem, InputLabel, Paper, FormControl, FormHelperText, FormLabel, CssBaseline } from '@mui/material';
+import { Alert, AppBar, Card, CardHeader, CardContent, Container, FormGroup, FormControlLabel, Grid, Select, type SelectChangeEvent, Switch, TextField, Toolbar, Typography, MenuItem, InputLabel, Paper, FormControl, FormHelperText, FormLabel, CssBaseline } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { parse as parseCsv } from 'csv-parse/browser/esm/sync';
 import { stringify as csvStringify } from 'csv-stringify/browser/esm/sync';
@@ -50,11 +51,44 @@ function InputData({ inputText, updateText, sourceFormats, selectedFormat, selec
           </FormControl>
         </Grid>
         <Grid item xs={12}>
-          <TextField value={inputText} onChange={e => updateText(e.target.value)} multiline rows={8} fullWidth />
+          <TextField value={inputText} onChange={e => updateText(e.target.value)} multiline rows={8} fullWidth InputProps={{ sx: { fontFamily: 'Inconsolata, monospace', tabSize: 4 } }} />
         </Grid>
       </Grid>
     </Paper>
   );
+}
+
+function validateJson(json: unknown) {
+  if (!Array.isArray(json)) {
+    return (
+      <Alert variant="filled" severity="error">
+            Top level JSON should be an array.
+      </Alert>
+    );
+  }
+
+  const validTypes = ['number', 'string', 'boolean'];
+  for (const entry of json) {
+    if (typeof entry !== 'object') {
+      return (
+        <Alert variant="filled" severity="error">
+            JSON should be an array of objects
+        </Alert>
+      );
+    }
+
+    for (const key of Object.keys(entry)) {
+      if (!validTypes.includes(typeof entry[key])) {
+        return (
+          <Alert variant="filled" severity="error">
+            Properties on the objects should be primitive types
+          </Alert>
+        );
+      }
+    }
+  }
+
+  return undefined;
 }
 
 interface TransformedDataProps {
@@ -66,60 +100,69 @@ interface TransformedDataProps {
 }
 function TransformedData({ data, targetFormats, sourceFormat, targetFormat, setTargetFormat }: TransformedDataProps) {
   const [pretty, setPretty] = useState(true);
+  let feedBackTextStatus = false;
   let parsedData;
   let convertedData;
+  let feedBackText;
   try {
     if (sourceFormat === 'CSV') {
       parsedData = parseCsv(data, { columns: true });
 
     } else if (sourceFormat === 'JSON') {
       parsedData = JSON.parse(data);
+      feedBackText = validateJson(parsedData);
+      if (feedBackText) {
+        feedBackTextStatus = true;
+      }
     } else {
-      throw Error(`Unknown source format type: ${sourceFormat}`);
-    }
+  throw Error(`Unknown source format type: ${sourceFormat}`);
+}
 
-    if (targetFormat === 'CSV') {
-      convertedData = csvStringify(parsedData, { header: true });
-    } else if (targetFormat === 'JSON') {
-      convertedData = JSON.stringify(parsedData, null, pretty ? '\t' : '');
-    } else {
-      throw Error(`Unknown source format type: ${sourceFormat}`);
-    }
+if (targetFormat === 'CSV') {
+  convertedData = csvStringify(parsedData, { header: true });
+} else if (targetFormat === 'JSON') {
+  convertedData = JSON.stringify(parsedData, null, pretty ? '\t' : '');
+} else {
+  throw Error(`Unknown source format type: ${sourceFormat}`);
+}
   } catch (error) {
-    convertedData = JSON.stringify(error, null, '\t');
-  }
+  convertedData = JSON.stringify(error, null, '\t');
+}
 
-  return (
-    <Paper elevation={6}>
-      <AppBar position="static" color="secondary">
-        <Toolbar>
-          <Typography variant="h5" component="h2">
-            Converted Data
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Grid container spacing={3} padding={2} alignItems="center">
-        <Grid item xs={12} container spacing={3} alignItems="center">
-          <Grid item>
-            <FormControl size="small" sx={{ minWidth: '5em' }}>
-              <FormLabel>Target Format</FormLabel>
-              <Select variant="outlined" value={targetFormat} onChange={e => setTargetFormat(e.target.value as FormatType)} >
-                {targetFormats.map(format => <MenuItem key={format} value={format}>{format}</MenuItem>)}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item>
-            <FormGroup>
-              <FormControlLabel control={<Switch checked={pretty} onChange={e => setPretty(e.target.checked)} />} label="Pretty" />
-            </FormGroup>
-          </Grid>
+return (
+  <Paper elevation={6}>
+    <AppBar position="static" color="secondary">
+      <Toolbar>
+        <Typography variant="h5" component="h2">
+          Converted Data
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Grid container spacing={3} padding={2} alignItems="center">
+      <Grid item xs={12} container spacing={3} alignItems="center">
+        <Grid item>
+          <FormControl size="small" sx={{ minWidth: '5em' }}>
+            <FormLabel>Target Format</FormLabel>
+            <Select variant="outlined" value={targetFormat} onChange={e => setTargetFormat(e.target.value as FormatType)} >
+              {targetFormats.map(format => <MenuItem key={format} value={format}>{format}</MenuItem>)}
+            </Select>
+          </FormControl>
         </Grid>
-        <Grid item xs={12}>
-          <TextField value={convertedData} multiline rows={8} fullWidth />
+        <Grid item>
+          <FormGroup>
+            <FormControlLabel control={<Switch checked={pretty} onChange={e => setPretty(e.target.checked)} />} label="Pretty" />
+          </FormGroup>
         </Grid>
       </Grid>
-    </Paper>
-  );
+      {feedBackTextStatus &&
+        <Grid item>{feedBackText}</Grid>
+      }
+      <Grid item xs={12}>
+        <TextField value={convertedData} multiline rows={8} fullWidth InputProps={{ sx: { fontFamily: 'Inconsolata, monospace', tabSize: 4 } }} />
+      </Grid>
+    </Grid>
+  </Paper>
+);
 }
 
 const Home: NextPage = () => {
@@ -144,7 +187,7 @@ const Home: NextPage = () => {
       <AppBar position="static" elevation={3} sx={{ mb: 2 }}>
         <Toolbar>
           <Typography variant="h4" component="h1">
-            Welcome to the Data Converter!
+            Welcome to the JSON/CSV Converter!
           </Typography>
         </Toolbar>
       </AppBar>
